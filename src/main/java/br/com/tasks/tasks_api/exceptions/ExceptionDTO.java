984a9487fb6e0875d06cb18f0445f5ddf68c396e
Luciano Gomes <lucianogvda02@gmail.com>
2024-06-04 21:19:30 -0300

    feat: implements exception handling for TaskController

---
+++ src/main/java/br/com/tasks/tasks_api/exceptions/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package br.com.tasks.tasks_api.exceptions;
+
+public record ExceptionDTO( String message, String statusCode ) {
+}
---
+++ src/main/java/br/com/tasks/tasks_api/exceptions/TaskControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package br.com.tasks.tasks_api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class TaskControllerExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementException(NoSuchElementException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Tarefa não encontrada", "404");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDTO);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Informações inválidas para atualização da Tarefa", "400");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDTO);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Informações inválidas para criação da Tarefa", "400");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDTO);
+    }
+}
